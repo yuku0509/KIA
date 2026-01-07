b89fc76392b07318bab8d40ba9f23510aa71b421
yoshidayuku <y.by0509yuku@gmail.com>
2026-01-07 22:58:08 +0900

    update4

--- src/public/custom-elements/main-app.js
+++ src/public/custom-elements/main-app.js
@@ -37,14 +37,14 @@ class SiteApp extends HTMLElement {
 
     // ★ここを、取得したURLに書き換えてください
     this.loadScripts([
-      'https://yuku0509.github.io/KIA/site-header.js',
-      'https://yuku0509.github.io/KIA/topphoto-section.js',
-      'https://yuku0509.github.io/KIA/story-section.js',
-      'https://yuku0509.github.io/KIA/business-section.js',
-      'https://yuku0509.github.io/KIA/lifestyle-section.js',
-      'https://yuku0509.github.io/KIA/company-section.js',
-      'https://yuku0509.github.io/KIA/contact-section.js',
-      'https://yuku0509.github.io/KIA/site-footer.js'
+      'https://yuku0509.github.io/KIA/src/public/custom-elements/site-header.js',
+      'https://yuku0509.github.io/KIA/src/public/custom-elements/topphoto-section.js',
+      'https://yuku0509.github.io/KIA/src/public/custom-elements/story-section.js',
+      'https://yuku0509.github.io/KIA/src/public/custom-elements/business-section.js',
+      'https://yuku0509.github.io/KIA/src/public/custom-elements/lifestyle-section.js',
+      'https://yuku0509.github.io/KIA/src/public/custom-elements/company-section.js',
+      'https://yuku0509.github.io/KIA/src/public/custom-elements/contact-section.js',
+      'https://yuku0509.github.io/KIA/src/public/custom-elements/site-footer.js'
     ]);
   }
 
