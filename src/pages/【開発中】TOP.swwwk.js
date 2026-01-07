d7c3eef65112032ff28819da42a54c17b6f5b321
yoshidayuku <y.by0509yuku@gmail.com>
2026-01-07 17:58:56 +0900

    update3

--- src/pages/【開発中】TOP.swwwk.js
+++ src/pages/【開発中】TOP.swwwk.js
@@ -1,5 +1,6 @@
 $w.onReady(function () {
 	// 以下はWixのカスタム要素のID名に合わせる
+    const siteApp = $w('#site-app');
     const topPhotoSection = $w('#topphoto-section');
     const storySection = $w('#story-section');
     const businessSection = $w('#business-section');
@@ -10,8 +11,9 @@ $w.onReady(function () {
     const SiteFooter = $w('#site-footer');
     const privacyPolicySection = $w('#privacy-policy-section');
 
-    // 強制的に属性をセット（UI設定を上書きします）
-    topPhotoSection.setAttribute('tag-name', 'topphoto-section');
+	// 強制的に属性をセット（UI設定を上書きします）
+    siteApp.setAttribute('tag-name', 'site-app');
+	topPhotoSection.setAttribute('tag-name', 'topphoto-section');
 	storySection.setAttribute('tag-name', 'story-section');
 	businessSection.setAttribute('tag-name', 'business-section');
     lifeStyleSection.setAttribute('tag-name', 'lifestyle-section');
